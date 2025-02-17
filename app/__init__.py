64c8e820fb9f5ea15822efc2e0586434ae28ed0a
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2025-02-17 23:22:50 +0200

    change directroy of tem and static and rendering main pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from .front_routes import bp
 
 
 def create_app():
     app = Flask(__name__)
+    app.register_blueprint(bp)
     return app
--- app/front_routes.py
+++ app/front_routes.py
@@ -1,13 +1,59 @@
 """
 Render the Main pages Women, Men and Kids
 """
-from flask import Blueprint
+from flask import Blueprint, render_template, abort
+from jinja2 import TemplateNotFound
 
-bp = Blueprint('bp', __name__, template_folder="/template", static_folder="/static")
+bp = Blueprint('bp', __name__, template_folder="template", static_folder="static")
+
 
 @bp.route('/')
 def welcom():
     """
-    render the welcome page route
-    :return:
+    Render Home page
+    :return: HTML Home page
+    :raises 404 if the template not found
     """
+    try:
+        return render_template("index.html")
+    except TemplateNotFound:
+        abort(404)
+
+
+@bp.route('/men')
+def men():
+    """
+    Render men page
+    :return: HTML men page
+    :raises 404 if the template not found
+    """
+    try:
+        return render_template("men.html")
+    except TemplateNotFound:
+        abort(404)
+
+
+@bp.route('/women')
+def women():
+    """
+    Render women page
+    :return: HTML women page
+    :raises 404 if the template not found
+    """
+    try:
+        return render_template("women.html")
+    except TemplateNotFound:
+        abort(404)
+
+
+@bp.route('/kids')
+def kid():
+    """
+    Render kids page
+    :return: HTML kids page
+    :raises 404 if the template not found
+    """
+    try:
+        return render_template("kids.html")
+    except TemplateNotFound:
+        abort(404)
