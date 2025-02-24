39561b6f324872b4ab9ea20aa861bd2ea6dfd583
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2025-02-24 22:28:49 +0200

    fixing filtring of data for both men and women sections

--- app/__init__.py
+++ app/__init__.py
@@ -14,10 +14,12 @@ def create_app():
     db.init_app(app)                # configure sqlalchemy instance to work with Flask app
 
     from .routes.men import men
+    from .routes.women import women
     from app.routes.Admin import admin
     from app.routes.front_routes import bp
 
     app.register_blueprint(bp)
+    app.register_blueprint(women)
     app.register_blueprint(men)
     app.register_blueprint(admin)
 
--- app/routes/women.py
+++ app/routes/women.py
@@ -1,17 +1,17 @@
 """
-routs belong to men products that client could use
+routs belong to women products that client could use
 """
 import json
 from flask import Blueprint, jsonify, request
 from ..models import Clothing, Top, Bottom
 from ..utils.helper import TopOrBottom, checkCorrectParameter
 
-men = Blueprint('Men', __name__, url_prefix='/api')
+women = Blueprint('Women', __name__, url_prefix='/api')
 Models = [Clothing, Top, Bottom]
 
 
-@men.route('/men')
-def product():
+@women.route('/women')
+def Wproduct():
     """
     represent all the available product on the storage
     if there is a query search base on the query if not just search all men cloth
@@ -20,36 +20,24 @@ def product():
     from app import db
     try:
 
-        # check if the request contain a json if not show all the cloth relate to men
         if not request.is_json:
 
-            # query all the product relate to men
-            all_product = Clothing.query.filter(Clothing.gender != 'women').all()
+            all_product = Clothing.query.filter(Clothing.gender == 'women').all()
             result = [arg.to_dict() for arg in all_product]
-
-            # json .dumps because the result list contain dict which have unserizable objct
             return json.dumps(result, default=lambda x: list(x) if isinstance(x, tuple) else str(x), indent=2), 200
 
         elif checkCorrectParameter(list(request.get_json()), lst=["length", "sleeve", "color", "company", "gender"]):
             holder = request.get_json(silent=True)
             query = db.session.query(Clothing)
-            filter_list = [Clothing.gender != 'women']
+            filter_list = [Clothing.gender != 'men']
 
             if TopOrBottom(holder) == "top":
 
-                # query using Top and Clothing Models
-                query = query.\
+                query = query(Clothing). \
                     join(Top, Top.clothing_id == Clothing.id, isouter=True)
 
-                # loop over all the dict
                 for key, val in holder.items():
-
-                    # check if one of the key belong to Clothing Model if so
                     if hasattr(Clothing, key):
-
-                        # getattr(clothing, key) ==> Clothing.Length
-                        # getattr(clothing, key) == val --> Clothing.Length == val (short)
-                        # add the condition to filter list to filter it
                         filter_list.append(getattr(Clothing, key) == val)
                     else:
                         filter_list.append(getattr(Top, key) == val)
@@ -58,7 +46,6 @@ def product():
                 result = [arg.to_dict() for arg in query]
                 return json.dumps(result, default=lambda x: list(x) if isinstance(x, tuple) else str(x), indent=2), 200
 
-            # query using the Bottom and Clothing Models
             query = db.session.query(Clothing). \
                 join(Bottom, Bottom.clothing_id == Clothing.id, isouter=True)
             for key, val in holder.items():
@@ -67,8 +54,6 @@ def product():
                 else:
                     filter_list.append(getattr(Bottom, key) == val)
 
-            # using *cond because using cond without astrickt raise error, you need to unpack it
-            # and because it is a list just use one astrikt
             query = query.filter(*filter_list).all()
             result = [arg.to_dict() for arg in query]
             return json.dumps(result, default=lambda x: list(x) if isinstance(x, tuple) else str(x), indent=2), 200
